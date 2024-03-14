acc80ffb240b4dc21f43087b2eec9b658cc07723
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-03-14 09:37:34 +0100

    updated structure for start screen

--- js/start-screen.js
+++ js/start-screen.js
@@ -2,7 +2,7 @@ let counter = 0;
 let game_Theme = new Audio('../audio/main-theme.mp3');
 
 function init(){
-    game_Theme.play();
+
 }
 
 
@@ -14,10 +14,10 @@ function start() {
 function toggleAudio(element){
     counter++;
     if (counter % 2){
-        element.src = "/img/icons/music-muted.svg";
-        game_Theme.pause();
-    } else {
         element.src = "/img/icons/music-toggle.svg";
         game_Theme.play();
+    } else {
+        element.src = "/img/icons/music-muted.svg";
+        game_Theme.pause();
     }
 }
